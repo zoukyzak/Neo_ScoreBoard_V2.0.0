5f33f7b3daa87e941b4ea7f59cabcc07a3eecb87
zouky <zoukyzak@gmail.com>
2022-02-22 19:44:36 +0100

    22/02/22 19h43 OBJS15 terminer

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9133,6 +9133,7 @@ namespace CPV9
             Affich_Descript_OBJ(11, "Att");
         }
 
+
         private void Button_Obj_Sec_Att_12_Click(object sender, RoutedEventArgs e)
         {
             Affich_Descript_OBJ(12, "Att");
@@ -9153,35 +9154,10 @@ namespace CPV9
             Affich_Descript_OBJ(18, "Att");
         }
 
+
         private void Button_Obj_Sec_Att_15_Click(object sender, RoutedEventArgs e)
         {
-            StackPanel_OBJSec_Att.Visibility = Visibility.Collapsed;
-            Label_OBJ_Sec_15_Att.Visibility = Visibility.Visible;
-            double x = ((Rapport_Taille_Y + Rapport_Taille_X) / 3);
-            Label_OBJ_SEC_15_Att_1.FontSize = 3 * x;
-            Label_OBJ_SEC_15_Att_2.FontSize = 2 * x;
-            Label_OBJ_SEC_15_Att_3.FontSize = 2 * x;
-            Label_OBJ_SEC_15_Att_4.FontSize = 2 * x;
-            Label_OBJ_SEC_15_Att_5.FontSize = 2 * x;
-            Button_Obj_Sec_15_Att_oui.FontSize = 2 * x;
-            Button_Obj_Sec_15_Att_oui.Width = 8 * Rapport_Taille_X;
-            Button_Obj_Sec_15_Att_oui.Height = 3 * Rapport_Taille_Y;
-            Button_Obj_Sec_15_Att_non.FontSize = 2 * x;
-            Button_Obj_Sec_15_Att_non.Width = 8 * Rapport_Taille_X;
-            Button_Obj_Sec_15_Att_non.Height = 3 * Rapport_Taille_Y;
-        }
-        private void Button_Obj_Sec_15_Att_oui_Click(object sender, RoutedEventArgs e)
-        {
-            Label_OBJ_Sec_15_Att.Visibility = Visibility.Collapsed;
-            StackPanel_OBJSec_Att.Visibility = Visibility.Visible;
-            StackPanel_Obj_Sec_Att_5.Visibility = Visibility.Hidden;
-            Choix_ObjS_Att = 15;
-            Select_ObjS_Attaquant(Choix_ObjS_Att);
-        }
-        private void Button_Obj_Sec_15_Att_non_Click(object sender, RoutedEventArgs e)
-        {
-            Label_OBJ_Sec_15_Att.Visibility = Visibility.Collapsed;
-            StackPanel_OBJSec_Att.Visibility = Visibility.Visible;
+            Affich_Descript_OBJ(15, "Att");
         }
 
         private void Button_Obj_Sec_Att_16_Click(object sender, RoutedEventArgs e)
@@ -9348,33 +9324,7 @@ namespace CPV9
 
         private void Button_Obj_Sec_Def_15_Click(object sender, RoutedEventArgs e)
         {
-            StackPanel_OBJSec_Def.Visibility = Visibility.Collapsed;
-            Label_OBJ_Sec_15_Def.Visibility = Visibility.Visible;
-            double x = ((Rapport_Taille_Y + Rapport_Taille_X) / 3);
-            Label_OBJ_SEC_15_Def_1.FontSize = 3 * x;
-            Label_OBJ_SEC_15_Def_2.FontSize = 2 * x;
-            Label_OBJ_SEC_15_Def_3.FontSize = 2 * x;
-            Label_OBJ_SEC_15_Def_4.FontSize = 2 * x;
-            Label_OBJ_SEC_15_Def_5.FontSize = 2 * x;
-            Button_Obj_Sec_15_Def_oui.FontSize = 2 * x;
-            Button_Obj_Sec_15_Def_oui.Width = 8 * Rapport_Taille_X;
-            Button_Obj_Sec_15_Def_oui.Height = 3 * Rapport_Taille_Y;
-            Button_Obj_Sec_15_Def_non.FontSize = 2 * x;
-            Button_Obj_Sec_15_Def_non.Width = 8 * Rapport_Taille_X;
-            Button_Obj_Sec_15_Def_non.Height = 3 * Rapport_Taille_Y;
-        }
-        private void Button_Obj_Sec_15_Def_oui_Click(object sender, RoutedEventArgs e)
-        {
-            Label_OBJ_Sec_15_Def.Visibility = Visibility.Collapsed;
-            StackPanel_OBJSec_Def.Visibility = Visibility.Visible;
-            StackPanel_Obj_Sec_Def_5.Visibility = Visibility.Hidden;
-            Choix_ObjS_Def = 15;
-            Select_ObjS_Defenseur(Choix_ObjS_Def);
-        }
-        private void Button_Obj_Sec_15_Def_non_Click(object sender, RoutedEventArgs e)
-        {
-            Label_OBJ_Sec_15_Def.Visibility = Visibility.Collapsed;
-            StackPanel_OBJSec_Def.Visibility = Visibility.Visible;
+            Affich_Descript_OBJ(15, "Def");
         }
 
         private void Button_Obj_Sec_Def_16_Click(object sender, RoutedEventArgs e)
